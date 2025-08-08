53ea314a9f8c7aa51254636b8c19e17cd99c3d68
Muralee2 <muralimvkaws1@gmail.com>
2025-08-08 16:31:56 +0530

    Update node_pool.tf

--- node_pool.tf
+++ node_pool.tf
@@ -2,15 +2,14 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-node-pool"
   cluster    = google_container_cluster.primary.name
   location   = var.region
+  node_count = 2
 
   node_config {
-    machine_type    = "e2-medium"
-    service_account = data.google_service_account.gke_workload_sa.email
+    machine_type = "e2-medium"
+    disk_size_gb = 80         # ↓ Reduce this
+    disk_type    = "pd-standard"  # ↓ Or change to standard disks
     oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/cloud-platform"
     ]
-    tags = ["gke-node"]
   }
-
-  initial_node_count = 2
 }
