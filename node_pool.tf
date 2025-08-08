c4266d9456c55fcdb295ee3ef789451fafd87b47
Muralee2 <muralimvkaws1@gmail.com>
2025-08-08 16:49:07 +0530

    Update node_pool.tf

--- node_pool.tf
+++ node_pool.tf
@@ -2,12 +2,12 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "primary-node-pool"
   cluster    = google_container_cluster.primary.name
   location   = var.region
-  node_count = 2
+  node_count = 1  # <--- Minimum count to reduce cost
 
   node_config {
-    machine_type = "e2-medium"
-    disk_size_gb = 50
-    disk_type    = "pd-standard"
+    machine_type = "e2-medium"  # smallest supported by GKE
+    disk_size_gb = 30           # fits in 30GB HDD free tier
+    disk_type    = "pd-standard"  # use HDD to match free quota
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform"
@@ -19,3 +19,4 @@ resource "google_container_node_pool" "primary_nodes" {
     auto_repair  = true
   }
 }
+
