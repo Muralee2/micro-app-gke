f825196f57d643c9d3060f876c1c2dea43910721
Muralee2 <muralimvkaws1@gmail.com>
2025-08-08 16:36:51 +0530

    Update node_pool.tf

--- node_pool.tf
+++ node_pool.tf
@@ -1,15 +1,12 @@
 resource "google_container_node_pool" "primary_nodes" {
-  name       = "primary-node-pool"
   cluster    = google_container_cluster.primary.name
-  location   = var.region
+  location   = "us-central1"
   node_count = 2
 
   node_config {
     machine_type = "e2-medium"
-    disk_size_gb = 80         # ↓ Reduce this
-    disk_type    = "pd-standard"  # ↓ Or change to standard disks
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    disk_size_gb = 50
+    disk_type    = "pd-standard"  # This avoids triggering SSD quota
+    ...
   }
 }
