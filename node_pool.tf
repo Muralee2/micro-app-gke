dfccdf97157745e0e9cbe44924f3b00126e1c334
Muralee2 <muralimvkaws1@gmail.com>
2025-08-08 16:40:28 +0530

    Update node_pool.tf

--- node_pool.tf
+++ node_pool.tf
@@ -1,12 +1,21 @@
 resource "google_container_node_pool" "primary_nodes" {
+  name       = "primary-node-pool"
   cluster    = google_container_cluster.primary.name
-  location   = "us-central1"
+  location   = var.region
   node_count = 2
 
   node_config {
     machine_type = "e2-medium"
     disk_size_gb = 50
-    disk_type    = "pd-standard"  # This avoids triggering SSD quota
-    ...
+    disk_type    = "pd-standard"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
+
+  management {
+    auto_upgrade = true
+    auto_repair  = true
   }
 }
