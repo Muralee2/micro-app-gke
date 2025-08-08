af6c7776bad13c9cf9fd4c55a84da0389e6a10aa
Muralee2 <muralimvkaws1@gmail.com>
2025-08-08 16:30:57 +0530

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,7 +1,6 @@
 resource "google_container_cluster" "primary" {
   name                     = var.cluster_name
   location                 = var.region
-  remove_default_node_pool = true
   initial_node_count       = 1
 
   network    = google_compute_network.vpc.id
